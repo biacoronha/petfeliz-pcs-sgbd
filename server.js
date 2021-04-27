f53a9884e158dc6ee89170791360336a6de83cce
Caio de Almeida Eppenstein de Carvalho <caio.carvalho@uniriotec.br>
2021-04-27 18:23:39 -0300

    Configuração inicial Mongodb
    Exemplo de burger

--- server.js
+++ server.js
@@ -1,7 +1,10 @@
 const app = require('./src/application');
+const db = require("./database/config");
+const mongoose = require("mongoose");
 
 const port = process.env.PORT || 3000;
 
+mongoose.connect(db.uri, { useNewUrlParser: true }).then(()=>console.log("funcionou"));
 app.listen(port, () => {
   console.log('Aplicação sendo executada na porta:', port);
 });
