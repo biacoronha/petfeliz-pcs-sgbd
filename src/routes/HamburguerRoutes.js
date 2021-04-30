2390337af16ac340f6b50e47edd76e981c9a0f6f
Caio de Almeida Eppenstein de Carvalho <caio.carvalho@uniriotec.br>
2021-04-30 17:41:08 -0300

    Remoção de classes inúteis

--- src/routes/HamburguerRoutes.js
+++
@@ -1,8 +0,0 @@
-const router = require('express-promise-router')();
-const HamburgerController = require("../controllers/HamburgerController");
-
-router.get("/hamburger", HamburgerController.index);
-
-router.post("/hamburger", HamburgerController.store);
-
-module.exports = router;
